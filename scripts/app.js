77e4155191bb7129b2e701c1af31ab649c3f1f32
Abhinavsaini004 <156031827+Abhinavsaini004@users.noreply.github.com>
2024-01-20 22:07:29 -0500

    week 2 comminted

--- scripts/app.js
+++ scripts/app.js
@@ -1,71 +1,81 @@
 "use strict";
-"use strict";
 
+(function () {
 
-(function (){
-
-    function DisplayHomePage(){
-
+    function DisplayHomePage() {
         console.log("Called DisplayHomePage()");
-        let AboutUsButton = document.getElementById("AboutUsBtn")
-        AboutUsButton.addEventListener("click", function (){
-            location.href="about.html";
-        })
 
+        // Redirect to the "about.html" page when the "About Us" button is clicked
+        let AboutUsButton = document.getElementById("DisplayHomePage");
+        AboutUsButton.addEventListener("click", function() {
+            window.location.href = "about.html";
+        });
+
+        // Create a new paragraph element
+        let MainParagraph = document.createElement("p");
         MainParagraph.setAttribute("id", "MainParagraph");
         MainParagraph.setAttribute("class", "mt-3");
-        MainParagraph.TextContent="This is my paragraph";
+        MainParagraph.textContent = "This is my paragraph";
+
+        // Get the main content element
+        let MainContent = document.querySelector("main");
+
+        // Append the paragraph to the main content
         MainContent.appendChild(MainParagraph);
 
-        let FirstString = " This is";
+        let FirstString = "This is";
         let SecondString = `${FirstString} the main paragraph`;
         MainParagraph.textContent = SecondString;
-        MainContent.appendChild(MainParagraph);
 
         let DocumentBody = document.body;
         let Article = document.createElement("article");
-        let ArticleParagraph =`<p id="ArticleParagraph" class="m-3"> This is my article paragraph</p>`;
-        Article.setAttribute("class","container");
-        Article.innerHTML = ArticleParagraph ;
+        let ArticleParagraph = `<p id="ArticleParagraph" class="m-3">This is my article paragraph</p>`;
+        Article.setAttribute("class", "container");
+        Article.innerHTML = ArticleParagraph;
         DocumentBody.appendChild(Article);
-
-
     }
-    function DisplayProductPage(){
+
+    function DisplayProductPage() {
 
         console.log("Called DisplayProductPage()");
     }
-    function DisplayAboutUsPage(){
+
+    function DisplayAboutUsPage() {
 
         console.log("Called DisplayAboutUsPage()");
     }
-    function DisplayContactPage(){
+
+    function DisplayContactPage() {
 
         console.log("Called DisplayContactPage()");
     }
-    function DisplayServicePage(){
+
+    function DisplayServicePage() {
 
         console.log("Called DisplayServicePage()");
     }
-    function Start(){
+
+    function Start() {
         console.log("App Started");
 
-        switch (document.title){
+        switch (document.title) {
             case "Home":
-                DisplayHomePage
+                DisplayHomePage();
                 break;
             case "Our Products":
-                DisplayProductPage
+                DisplayProductPage();
                 break;
             case "About Us":
-                DisplayAboutUsPage
+                DisplayAboutUsPage();
                 break;
             case "Our Services":
-                DisplayServicePagePage
+                DisplayServicePage();
                 break;
             case "Contact Us":
+                DisplayContactPage();
                 break;
         }
     }
+
     window.addEventListener("load", Start);
-})()
+})();
